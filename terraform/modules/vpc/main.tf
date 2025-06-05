0036c225571090705393dc1337eab85ceb4fba1f
adilarrad <adil.arrad@gmail.com>
2025-06-05 02:41:06 +0100

    internet-gw + route-table

--- terraform/modules/vpc/main.tf
+++ terraform/modules/vpc/main.tf
@@ -14,3 +14,29 @@ resource "aws_subnet" "subnet" {
     Name = "Project-Subnet"
   }
 }
+
+resource "aws_internet_gateway" "gw" {
+  vpc_id = aws_vpc.vpc.id
+
+  tags = {
+    Name = "Project-Internet-Gateway"
+  }
+}
+
+resource "aws_route_table" "route" {
+  vpc_id = aws_vpc.vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.gw.id
+  }
+
+  tags = {
+    Name = "Project-Route-Table"
+  }
+}
+
+resource "aws_route_table_association" "a" {
+  subnet_id      = aws_subnet.subnet.id
+  route_table_id = aws_route_table.route.id
+}
